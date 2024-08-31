b042281c74c59b3c805957b377a1b2cb0918d6fa
oppen321 <zj18139624826@gmail.com>
2024-08-31 22:04:37 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -3,6 +3,15 @@
 # 修改默认IP
 #sed -i 's/192.168.100.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 
+# Git稀疏克隆，只克隆指定目录到本地
+function git_sparse_clone() {
+  branch="$1" repourl="$2" && shift 2
+  git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
+  repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
+  cd $repodir && git sparse-checkout set $@
+  mv -f $@ ../package
+  cd .. && rm -rf $repodir
+}
 
 
 
