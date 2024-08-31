7b63f1cb5a1b7c3db0db03c48ddd27badf48293a
oppen321 <zj18139624826@gmail.com>
2024-08-31 22:18:31 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -13,12 +13,12 @@ function git_sparse_clone() {
   cd .. && rm -rf $repodir
 }
 
-git clone --depth=1 https://github.com/linkease/istore-packages.git;main package/istore-packages
-git clone --depth=1 https://github.com/jjm2473/luci-app-diskman.git;dev package/diskman
-git clone --depth=1 https://github.com/jjm2473/OpenAppFilter.git;dev4 package/oaf
-git clone --depth=1 https://github.com/linkease/nas-packages.git;master package/nas-packages
-git clone --depth=1 https://github.com/linkease/nas-packages-luci.git;main package/nas-packages-luci
-git clone --depth=1 https://github.com/jjm2473/openwrt-apps.git;main package/openwrt-apps
+git clone --depth=1 https://github.com/linkease/istore-packages;main package/istore-packages
+git clone --depth=1 https://github.com/jjm2473/luci-app-diskman;dev package/diskman
+git clone --depth=1 https://github.com/jjm2473/OpenAppFilter;dev4 package/oaf
+git clone --depth=1 https://github.com/linkease/nas-packages;master package/nas-packages
+git clone --depth=1 https://github.com/linkease/nas-packages-luci;main package/nas-packages-luci
+git clone --depth=1 https://github.com/jjm2473/openwrt-apps;main package/openwrt-apps
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
