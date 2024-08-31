f8c2f3ca38895336fe963d6769ee382c4a0cb833
oppen321 <zj18139624826@gmail.com>
2024-08-31 15:05:20 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # 修改默认IP
-sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.100.1/10.0.0.1/g' package/base-files/files/bin/config_generate
