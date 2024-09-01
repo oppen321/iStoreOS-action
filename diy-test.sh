0bb4f9e93d05d17eeb10c6e6273e8cd041c157c2
oppen321 <zj18139624826@gmail.com>
2024-09-01 14:06:23 +0800

    Update diy-test.sh

--- diy-test.sh
+++ diy-test.sh
@@ -10,7 +10,7 @@ rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing-box
 rm -rf feeds/packages/utils/v2dat
 rm -rf feeds/packages/lang/golang
 rm -rf feeds/luci/applications/luci-app-mosdns
-
+rm -rf feeds/packages/net/v2ray-geodata
 
 # Git稀疏克隆，只克隆指定目录到本地
 function git_sparse_clone() {
@@ -25,6 +25,13 @@ function git_sparse_clone() {
 # 科学上网插件
 git_sparse_clone master https://github.com/vernesong/OpenClash luci-app-openclash
 
+# AdGuardHome
+git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
+
+# Modns
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+
 # iStoreOS 官方插件库
 git clone --depth=1 -b main https://github.com/linkease/istore-packages package/istore-packages
 git clone --depth=1 -b dev https://github.com/jjm2473/luci-app-diskman package/diskman
@@ -32,6 +39,9 @@ git clone --depth=1 -b dev4 https://github.com/jjm2473/OpenAppFilter package/oaf
 git clone --depth=1 -b master https://github.com/linkease/nas-packages package/nas-packages
 git clone --depth=1 -b main https://github.com/linkease/nas-packages-luci package/nas-packages-luci
 git clone --depth=1 -b main https://github.com/jjm2473/openwrt-apps package/openwrt-apps
+
+# 更换golong 1.23
+git clone https://github.com/oppen321/golang feeds/packages/lang/golang
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
